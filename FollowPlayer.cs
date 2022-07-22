e924ab33f653c48934d3c887ae509453e11411ba
April Speight <april.a.speight@gmail.com>
2022-07-22 16:28:54 -0700

    Create FollowPlayer.cs

---
+++ FollowPlayer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    // Offsets the camera to be above the player
+    private Vector3 offset = new Vector3(0, 5, -7);
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // Offset the camera behind the player by adding to the player's position
+        transform.position = player.transform.position + offset;
+    }
+}
